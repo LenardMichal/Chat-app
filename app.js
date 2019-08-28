850e9c7c77813f90b0112aa799555c79d8cc837d
Michał Lenard <lenard.michal@yahoo.com>
2019-08-28 13:21:36 +0200

    Added basic socket handlers

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const app = express();
+// const cors = require('cors');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+// const { message, setUserName, userNameOverload } = require('./socket-IO');
+
+// app.use(cors());
+
+let globalUserIndex = 1;
+let userList = [];
+
+// Setup view engine
+app.set('view engine', 'ejs');
+
+
 
 
 app.get('/', (req, res) => {
-  res.send("Hiho!");
+  res.render('index');
+});
+
+// Io setup
+io.on('connection', socket => {
+  if(!socket.client.userName){
+    userList.push(`user${globalUserIndex}`);
+    socket.client.userName = `user${globalUserIndex}`;
+    globalUserIndex++;
+  }
+
+  
+  socket.emit('greeting', {message: `Hello there is ${userList.length} poeple.`});
+  
+  socket.on('message', (data) => {
+    socket.emit('message', {user: socket.client.userName, message: data});
+  })
+
+  
+
+
+  socket.on('disconnect', () => {
+   userList = userList.filter((user) => {
+     return user !== socket.client.userName
+   })
+  })  
 })
 
+server.listen(80);
 
-module.exports = app;
+module.exports = io;
